1e07a55dfe201eebef34d7172c25e85c64bc7d34
Anton <Anton.Rudakov216@mail.ru>
2022-10-12 16:33:26 +0300

    update

--- geek.cpp
+++ geek.cpp
@@ -1,209 +1,108 @@
 #include <iostream>
-#include <cmath>
 #include "geek.hpp"
 
-// Дерево
+// Граф
 
-Tree::Tree(){
-    root = nullptr;
-    countNode = depth = 0;
+Graph::Graph(): size(0){}
+
+Graph::Graph(const int SIZE, int mtrx[]){
+    init(SIZE, mtrx);
 }
 
-// Создаёт и инициализирует элемент дерева
-PNode createNode(const int data){
-    PNode ptr = new TNode;
-    ptr->Data = data;
-    ptr->Left = ptr->Right = ptr->Parent = nullptr;
-    return ptr;
-}
-
-// Перевод строки в дерево
-void putNodeStr(PNode ptr, std::string &str, int &count){
-    int data = str[count] - 48;
-    ptr->Data = data;
-    if(str[++count] == '('){
-        if(str[++count] != ','){
-            --count;
-            PNode pl = createNode(0);
-            ptr->Left = pl;
-            putNodeStr(pl, str, ++count);
-        }
-        if(str[count] == ','){
-            PNode pr = createNode(0);
-            ptr->Right = pr;
-            putNodeStr(pr, str, ++count);
-        }
-        ++count;
-    }
-}
-
-void Tree::putNode(PNode ptr, const int data){
-    if(data < ptr->Data){
-        if(ptr->Left){
-            putNode(ptr->Left, data);
-        }else{
-            ptr->Left = createNode(data);
-            countNode++;
-        }
-    }else{
-        if(ptr->Right){
-            putNode(ptr->Right, data);
-        }else{
-            ptr->Right = createNode(data);
-            countNode++;
-        }
-    }
-}
-
-// Проверка баланса скобок
-bool checkingBrackets(std::string &str){
-    if(str.length() == 0) return false;
-    Stack stk;
-    for(int i = 0; i < str.length(); ++i){
-        if(str[i] == '[' || str[i] == '(' || str[i] == '{'){
-            if(str[i] == '[') stk.push(']');
-            if(str[i] == '(') stk.push(')');
-            if(str[i] == '{') stk.push('}');
-            continue;
-        }
-        if(str[i] == ']' || str[i] == ')' || str[i] == '}'){
-            if(stk.pop() != str[i]) return false;
-            continue;
-        }
-    }
-    return true;
-}
-
-Tree::Tree(std::string str){
-    if(!checkingBrackets(str)){
-        std::cerr << "Error brackets!!!\n";
-        return;
-    }
-    int count = 0;
-    root = createNode(0);
-    putNodeStr(root, str, count);
-}
-
-Tree::~Tree(){
+Graph::~Graph(){
     clear();
 }
 
-void Tree::push(const int data){
-    if(countNode == 0){
-        root = createNode(data);
-        countNode++;
-    }else{
-        putNode(root, data);
+void Graph::init(const int SIZE, int mtrx[]){
+    size = SIZE;
+    matrix = new int*[size];
+    for(int j = 0; j < size; j++){
+        matrix[j] = new int[size];
+        for(int i = 0; i < size; i++){
+            matrix[j][i] = mtrx[j * SIZE + i];
+        }
     }
 }
 
-// Удаляет элемент дерева, а так же все его дочерние вершины
-void eraseBranch(PNode ptr){
-    if(ptr->Left) eraseBranch(ptr->Left);
-    if(ptr->Right) eraseBranch(ptr->Right);
-    delete ptr;
+void Graph::clear(){
+    if(size == 0) return;
+    for(int i = 0; i < size; ++i)
+        delete[] matrix[i];
+    delete[] matrix;
+    size = 0;
 }
 
-void Tree::clear(){
-    if(countNode == 0) return;
-    eraseBranch(root);
-    root = nullptr;
-    countNode = depth = 0;
+void Graph::recurPrinDepth(const int index, int* visited, Stack* stk){
+    stk->push(index);
+    visited[index] = 1;
+    for(int next = 0; next < size; ++next)
+        if(matrix[index][next] == 1 && !visited[next])
+            recurPrinDepth(next, visited, stk);
 }
 
-PNode Tree::getRoot(){
-    return root;
-}
-
-int Tree::getCountNode(){
-    return countNode;
-}
-
-// Обновляет значение глубины дерева
-int updateDepth(PNode ptr, const int count){
-    if(!ptr) return count;
-    int maxL = updateDepth(ptr->Left, count + 1);
-    int maxR = updateDepth(ptr->Right, count + 1);
-    return (maxL > maxR ? maxL : maxR);
-}
-
-int Tree::getDepth(){
-    depth = updateDepth(root, 0);
-    return depth;
-}
-
-void Tree::nodeToStr(PNode ptr, const int count, std::string* str){
-    // Заполнение пробелами пустых поддеревьев
-    if(!ptr){
-        for(int i = count; i < depth; ++i)
-            str[i]+= std::string(pow(2, depth - count - 1) * 2, ' ');
-        return;
-    }
-    nodeToStr(ptr->Left, count + 1, str);
-    int delta = pow(2, depth - count - 1); // Длина печати от края и до элемента
-
-    // Отрисовка левой стрелки
-    if(ptr->Left){
-        str[count]+= std::string((delta - 1) / 2, ' ');
-        if(count < depth - 1) str[count]+='+';
-        str[count]+= std::string((delta - 1) / 2, '-');
-    }else{
-        str[count]+= std::string(delta - 1, ' ');
-    }
-
-    str[count]+= char(ptr->Data + 48);
-
-    // Отрисовка правой стрелки
-    if(ptr->Right){
-        str[count]+= std::string(delta / 2 - 1, '-');
-        if(count < depth - 1) str[count]+='+';
-        str[count]+= std::string(delta / 2, ' ');
-    }else{
-        str[count]+= std::string(delta, ' ');
-    }
-    nodeToStr(ptr->Right, count + 1, str);
-}
-
-void Tree::printDiagram(){
-    depth = updateDepth(root, 0);
-    if(depth == 0) return;
-    std::string* str = new std::string[depth];
-    for(int i = 0; i < depth; ++i) str[i] = "";
-    nodeToStr(root, 0, str);
-    for(int i = 0; i < depth; ++i) std::cout << str[i] << std::endl;
-    delete[] str;
-}
-
-// 
-void prin(PNode ptr){
-    if(ptr->Left) prin(ptr->Left);
-    std::cout << ptr->Data << ' ';
-    if(ptr->Right) prin(ptr->Right);
-}
-
-void Tree::print(){
-    if(root) prin(root);
+void Graph::printDepth(){
+    int* visited = new int[size];
+    for(int i = 0; i < size; ++i)
+        visited[i] = 0;
+    Stack stk;
+    recurPrinDepth(0, visited, &stk);
+    std::cout << "Verts: ";
+    for(int i = 0; i < size; ++i)
+        visited[i] = stk.pop();
+    for(int i = size - 1; i >= 0; --i)
+        std::cout << visited[i] << ' ';
     std::cout << std::endl;
+    delete[] visited;
 }
 
-// 
-int countN(PNode ptr, bool& flag){
-    if(!ptr) return 0;
-    int left = countN(ptr->Left, flag);
-    int right = countN(ptr->Right, flag);
-    if(abs(left - right) > 1) flag = false;
-    return left + right + 1;
+void Graph::graphTraversalRecur(const int id, int* visited, NodeToPrint* arr){
+    visited[id] = 1;
+    arr[id].index = id;
+    for(int i = 0; i < size; ++i) if(matrix[id][i] == 1) arr[id].numberLinks++;
+    for(int i = 0; i < size; ++i) if(matrix[id][i] == 1 && !visited[i]) graphTraversalRecur(id + 1, visited, arr);
 }
 
-bool Tree::isBalanced(){
-    if(countNode == 0) return false;
-    bool flag = true;
-    countN(root, flag);
-    return flag;
+void Graph::graphTraversalMatrix(NodeToPrint* arr){
+    for(int i = 0; i < size; ++i){
+        arr[i].index = i;
+        arr[i].numberLinks = 0;
+        for(int j = 0; j < size; ++j)
+            if(matrix[i][j]) arr[i].numberLinks++;
+    }
+}
+
+void Graph::printTraversal(const int dir){
+    if(size == 0) return;
+    NodeToPrint* arr = new NodeToPrint[size];
+    for(int i = 0; i < size; ++i){
+        arr[i].index = -1;
+        arr[i].numberLinks = 0;
+    }
+
+    int index = 0;
+    int* visited = new int[size];
+    for(int i = 0; i< size; ++i) visited[i] = 0;
+    switch(dir){
+        case 0: graphTraversalMatrix(arr); break;
+        case 1: graphTraversalRecur(index, visited, arr); break;
+        default: return; break;
+    }
+    delete[] visited;
+
+    for(int i = 0; i < size; ++i)
+        for(int j = 0; j < size; ++j)
+            if(arr[i].numberLinks > arr[j].numberLinks)
+            std::swap(arr[i], arr[j]);
+
+    for(int i = 0; i < size; ++ i)
+        if(arr[i].index >= 0)
+            std::cout << "Varts: " << arr[i].index << " - links: " << arr[i].numberLinks << std::endl;
+    
+    delete[] arr;
 }
 
 // ==============================================
-// Стек для проверки баланса скобок
+// Стек
 
 Stack::Stack(){
     size = 0;
@@ -214,7 +113,7 @@ Stack::~Stack(){
     clear();
 }
 
-void Stack::push(char data){
+void Stack::push(int data){
     node* it = new node;
     it->data = data;
     it->next = head;
@@ -222,7 +121,7 @@ void Stack::push(char data){
     size++;
 }
 
-char Stack::pop(){
+int Stack::pop(){
     if(size == 0) return '_';
     node* it = head;
     head = head->next;
@@ -232,7 +131,7 @@ char Stack::pop(){
     return data;
 }
 
-char Stack::peek(){
+int Stack::peek(){
     if(size == 0) return '_';
     return head->data;
 }
--- geek.hpp
+++ geek.hpp
@@ -1,58 +1,12 @@
 #pragma once
 // На вводном уроке было сказано, что файл "geek" можно реализовать самому
 
-struct TNode{
-    int Data;
-    TNode* Left;
-    TNode* Right;
-    TNode* Parent;
-};
-
-typedef TNode* PNode;
-
-// Дерево
-class Tree{
-private:
-    PNode root; // Указатель на корень дерева
-    int countNode, depth; // Количество элементов в дереве и его глубина
-    // Поиск места в дереве и размещение элемента
-    void putNode(PNode ptr, const int data);
-    // Подготовка строк перед отрисовкой диаграмы
-    void nodeToStr(PNode ptr, const int count, std::string* str);
-public:
-    // Конструктор пустого дерева
-    Tree();
-    // Конструктор дерева через строку
-    Tree(std::string str);
-    // Деструктор
-    ~Tree();
-    // Вставляет элемент
-    void push(const int data);
-    // Очищает дерево
-    void clear();
-    // Возвращает указатель на корень дерева
-    PNode getRoot();
-    // Возвращает количество нодов в дереве
-    int getCountNode();
-    // Возвращает глубину дерева
-    int getDepth();
-    // Печать в терминал диаграмы дерева
-    void printDiagram();
-    // Печать элементов дерева в консоли в инфиксном порядке
-    void print();
-    // Task 1
-    // Написать функцию проверяющую является ли бинарное дерево сбалансированным
-    bool isBalanced();
-};
-
-// ==============================================
-// Стек для проверки баланса скобок
-
+// Стек
 class Stack{
 private:
     // Элемент стека
     struct node{
-        char data; // Данные элемента
+        int data; // Данные элемента
         node* next; // Следующий элемент стека
     } *head; // Указатель на вершину стека
     int size; // размер стека
@@ -62,15 +16,51 @@ public:
     // Деструктор
     ~Stack();
     // Добавляет элемент со знаением data
-    void push(char data);
+    void push(int data);
     // Возвращает значение вершины стека и извлекает элемент
-    char pop();
+    int pop();
     // Возвращает значение вершины стека
-    char peek();
+    int peek();
     // Очищает стек
     void clear();
     // Печать стека
     void print();
     // Возвращает размер стэка
     int getSize();
+};
+
+// ==============================================
+
+// Граф
+class Graph{
+private:
+    int size; // Количество элементов в графе
+    int** matrix; // Матрица смежности
+    struct NodeToPrint{
+        int index;
+        int numberLinks;
+    };
+    // Рекурсивная функция обхода графа в глубину
+    void recurPrinDepth(const int index, int* visited, Stack* stk);
+    // Обход графа рекурсивной функцией (с подсчётом только смежных со стартовой вершин)
+    void graphTraversalRecur(const int id, int* visited, NodeToPrint* arr);
+    // Обход графа по матрице смежности (с подсчётом всех вершин графа)
+    void graphTraversalMatrix(NodeToPrint* arr);
+public:
+    // Конструктор пустого графа
+    Graph();
+    // Конструктор графа размера SIZE с данными из матрицы смежности mtrx
+    Graph(const int SIZE, int mtrx[]);
+    // Деструктор
+    ~Graph();
+    // Инициализирует граф размера SIZE с данными из матрицы смежности mtrx
+    void init(const int SIZE, int mtrx[]);
+    // Очищает граф
+    void clear();
+    // Печать в терминал индексов вершин графа в глубину
+    void printDepth();
+    // списка всех узлов в порядке уменьшения количества ссылок на них
+    // 0 - Обход графа по матрице смежности (с подсчётом всех вершин графа)
+    // 1 - Обход графа рекурсивной функцией (с подсчётом только смежных со стартовой вершин)
+    void printTraversal(const int dir);
 };
